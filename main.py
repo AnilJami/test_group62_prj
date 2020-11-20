996a523abcb45193899f9846de1f262b84d327e7
Anil Jami <anil.jami@oportun.com>
2020-11-19 18:57:00 -0800

    v1

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ import psycopg2
 conn = psycopg2.connect(database="test_db", user = "test", password = "test", host = "127.0.0.1", port = "5432")
 
 print ("Opened database successfully")
+
+cur = conn.cursor()
+
+cur.execute("SELECT id, name from test_schema.test_table")
+rows = cur.fetchall()
+for row in rows:
+   print "ID = ", row[0]
+   print "NAME = ", row[1], "\n"
+
+print "Operation done successfully";
+conn.close()
